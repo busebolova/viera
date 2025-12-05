27b4268224608dcb1d59295cfbc009d1c6eefaa7
busebolova <busebolovarslan@gmail.com>
2025-12-05 16:40:37 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
-import ClientLayout from "./client-layout"
+import "./globals.css"
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Viera",
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>
-        <ClientLayout>
-          {children}
-        </ClientLayout>
+    <html lang="tr">
+      <body className={inter.className}>
+        {children}
       </body>
     </html>
   )
