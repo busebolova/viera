ff55fb9f0d798f85fa17b10c7ea52854752b5c2c
busebolova <busebolovarslan@gmail.com>
2025-12-05 15:37:59 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import { HomeClient } from "./HomeClient"
-
-export const metadata = {
-  title: "VIERA - Alkan Yapı & Viera Ortaklığı | Üsküdar İnşaat Firması - 60+ Yıllık Tecrübe",
-  description:
-    "VIERA - Alkan Yapı & Viera Ortaklığı, Üsküdar merkezli 60 yılı aşkın deneyimle konut projeleri, ticari binalar ve karma kullanımlı yapı projeleri. İstanbul'un güvenilir inşaat ve müteahhitlik firması.",
-}
-
-export default function HomePage() {
-  return <HomeClient />
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Site çalışıyor ✔️</h1>
+    </main>
+  );
 }
